a318c0406071f5f4ed048ec193cd4539e4ceaaeb
countgrey <60135127+countgrey@users.noreply.github.com>
2024-09-02 15:54:33 +0500

    Add files via upload

---
+++ cowsay2.py
@@ -0,0 +1,34 @@
+import sys
+
+args = sys.argv
+flags = []
+for arg in args:
+	if "-" in arg:
+		flags.append(arg)
+	else:
+		coolText = arg
+
+str_len = len(coolText)
+eyes = "oo"
+mouth = "__"
+ears = "^__^"
+tail = "\\/\\"
+tonque = ""
+
+if "-p" in flags: eyes = "@@"
+if "-a" in flags: mouth = "/\\"
+if "-e" in flags: ears = "/\\/\\"
+if "-d" in flags: eyes = "$$"
+if "-t" in flags: tail = "-------------->"
+if "-to" in flags: tonque = "U"
+
+print(f'''
+   {"_" * len(coolText)}
+< {coolText} >
+   {"-" * len(coolText)}''')
+print(" " * (str_len//2), f"\\  {ears}")
+print(" " * (str_len//2), f" \\ ({eyes})\\________")
+print(" " * (str_len//2), f"   ({mouth})\\        ){tail}")
+print(" " * (str_len//2), f"     {tonque} ||------w|")
+print(" " * (str_len//2), "       ||      ||")
+
